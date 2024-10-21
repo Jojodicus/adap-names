b4d3e4adbf19687c68833221d2a30739e0e76b49
jojodicus <jojodicus@gmail.com>
2024-10-21 18:57:44 +0200

    methodtype annotation i have no clue what this is used for...

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -13,30 +13,37 @@ export class Name {
         }
     }
 
+    // @methodtype conversion-method
     public asNameString(delimiter: string = this.delimiter): string {
         return this.components.join(delimiter);
     }
 
+    // @methodtype get-method
     public getComponent(i: number): string {
         return this.components[i];
     }
 
+    // @methodtype set-method
     public setComponent(i: number, c: string): void {
         this.components[i] = c;
     }
 
+    // @methodtype get-method
     public getNoComponents(): number {
         return this.components.length;
     }
 
+    // @methodtype command-method
     public insert(i: number, c: string): void {
         this.components.splice(i, 0, c);
     }
 
+    // @methodtype command-method
     public append(c: string): void {
         this.components.push(c);
     }
 
+    // @methodtype command-method
     public remove(i: number): void {
         this.components.splice(i);
     }
