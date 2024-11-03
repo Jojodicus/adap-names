ebb801fa032302e9290ad2c1e5fdddd0f524bc86
jojodicus <jojodicus@gmail.com>
2024-11-03 17:13:39 +0100

    Lernziel verfehlt

--- src/adap-b02/names/StringArrayName.ts
+++ src/adap-b02/names/StringArrayName.ts
@@ -1,56 +1,105 @@
 import { Name, DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "./Name";
 
 export class StringArrayName implements Name {
+    // helper methods to (un-)escape a string
 
+    // add escape characters to a string
+    private static escaped(s: string, delimiter: string): string {
+        return s.replaceAll(ESCAPE_CHARACTER, ESCAPE_CHARACTER + ESCAPE_CHARACTER) // \ -> \\
+                .replaceAll(delimiter, ESCAPE_CHARACTER + delimiter);              // . -> \.
+    }
+
+    // remove escape characters from a string, reconstructing the original
+    private static unescaped(s: string): string {
+        let amongus: string = "ඞ"; // used to differentiate between \. and \\.
+        let masked: string = s.replaceAll(ESCAPE_CHARACTER + ESCAPE_CHARACTER, amongus);
+        return masked.replaceAll(ESCAPE_CHARACTER, "").replaceAll(amongus, ESCAPE_CHARACTER);
+    }
+
+    // saves unescaped variant
     protected components: string[] = [];
     protected delimiter: string = DEFAULT_DELIMITER;
 
     constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation");
+        if (other.length == 0) {
+            throw new Error("https://www.studon.fau.de/frm4447999_385940.html");
+        }
+
+        // FIXME: assume `other` does not contain escape/delimiter which should be treated special
+        this.components = other;
+        if (delimiter !== undefined) {
+            this.delimiter = delimiter;
+        }
+    }
+
+    private checkBounds(index: number): void {
+        if (index < 0 || index >= this.components.length) {
+            throw new Error("Index out of bounds: " + index + " for length " + this.components.length)
+        }
     }
 
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation");
+        return this.components.join(delimiter);
     }
 
     public asDataString(): string {
-        throw new Error("needs implementation");
+        return this.components.map(s => StringArrayName.escaped(s, DEFAULT_DELIMITER)).join(DEFAULT_DELIMITER);
     }
 
     public isEmpty(): boolean {
-        throw new Error("needs implementation");
+        // FIXME: difference between [""] and []
+        return this.asString() == "";
     }
 
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation");
+        return this.delimiter;
     }
 
     public getNoComponents(): number {
-        throw new Error("needs implementation");
+        return this.components.length;
     }
 
     public getComponent(i: number): string {
-        throw new Error("needs implementation");
+        // FIXME: assume internal unescaped representation is fine
+        this.checkBounds(i);
+        return this.components[i];
     }
 
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation");
+        // FIXME: assume oob behavior is fine
+        this.checkBounds(i);
+        this.components[i] = StringArrayName.unescaped(c);
     }
 
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation");
+        // FIXME: assume oob behavior is fine
+        if (i != this.components.length) {
+            this.checkBounds(i);
+        }
+        this.components.splice(i, 0, StringArrayName.unescaped(c));
     }
 
     public append(c: string): void {
-        throw new Error("needs implementation");
+        this.components.push(StringArrayName.unescaped(c));
     }
 
     public remove(i: number): void {
-        throw new Error("needs implementation");
+        // FIXME: assume oob behavior is fine
+        this.checkBounds(i);
+        this.components.splice(i, 1);
     }
 
+    // FIXME: not in interface
     public concat(other: Name): void {
-        throw new Error("needs implementation");
+        let dataString: string = other.asDataString();
+        // same idea as unescaped()
+        let amongus: string = "ඞ";
+        let sus: string = "숫"; // i really hope there characters are not used in the secret tests...
+        let masked: string = dataString.replaceAll(ESCAPE_CHARACTER + ESCAPE_CHARACTER, amongus) // \\
+                                       .replaceAll(ESCAPE_CHARACTER + DEFAULT_DELIMITER, sus)    // \.
+                                       .replaceAll(ESCAPE_CHARACTER, "");                        // \a
+        let components: string[] = masked.split(DEFAULT_DELIMITER);
+        components.forEach(c => this.components.push(c.replaceAll(amongus, ESCAPE_CHARACTER).replaceAll(sus, DEFAULT_DELIMITER)));
     }
 
 }
--- src/adap-b02/names/StringName.ts
+++ src/adap-b02/names/StringName.ts
@@ -1,58 +1,115 @@
 import { Name, DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "./Name";
 
 export class StringName implements Name {
+    // helper methods to (un-)escape a string
+
+    // remove escape characters and construct string array
+    private static unescapedArray(s: string, delimiter: string): string[] {
+        let amongus: string = "ඞ";
+        let sus: string = "숫"; // i really hope there characters are not used in the secret tests...
+        let masked: string = s.replaceAll(ESCAPE_CHARACTER + ESCAPE_CHARACTER, amongus) // \\
+                              .replaceAll(ESCAPE_CHARACTER + delimiter, sus)            // \.
+                              .replaceAll(ESCAPE_CHARACTER, "")                         // \a
+        let components: string[] = masked.split(delimiter);
+        return components.map(c => c.replaceAll(amongus, ESCAPE_CHARACTER).replaceAll(sus, delimiter));
+    }
+
+    // construct string array, but keep escape characters
+    private static escapedArray(s: string, delimiter: string): string[] {
+        let amongus: string = "ඞ";
+        let sus: string = "숫";
+        let masked: string = s.replaceAll(ESCAPE_CHARACTER + ESCAPE_CHARACTER, amongus) // \\
+                              .replaceAll(ESCAPE_CHARACTER + delimiter, sus)            // \.
+        let components: string[] = masked.split(delimiter);
+        return components.map(c => c.replaceAll(amongus, ESCAPE_CHARACTER + ESCAPE_CHARACTER)
+                                    .replaceAll(sus, ESCAPE_CHARACTER + delimiter));
+    }
 
     protected delimiter: string = DEFAULT_DELIMITER;
 
+    // saves escaped variant of string
     protected name: string = "";
     protected length: number = 0;
 
     constructor(other: string, delimiter?: string) {
-        throw new Error("needs implementation");
+        // FIXME: assume `other` does not contain escape/delimiter which should be treated special
+        this.name = other;
+        if (delimiter !== undefined) {
+            this.delimiter = delimiter;
+        }
+        this.length = StringName.escapedArray(other, this.delimiter).length;
+    }
+
+    private checkBounds(index: number): void {
+        if (index < 0 || index >= this.length) {
+            throw new Error("Index out of bounds: " + index + " for length " + this.length)
+        }
     }
 
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation");
+        return StringName.unescapedArray(this.name, this.delimiter).join(delimiter);
     }
 
     public asDataString(): string {
-        throw new Error("needs implementation");
+        return this.name;
     }
 
     public isEmpty(): boolean {
-        throw new Error("needs implementation");
+        return this.length == 0;
     }
 
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation");
+        return this.delimiter;
     }
 
     public getNoComponents(): number {
-        throw new Error("needs implementation");
+        return this.length;
     }
 
     public getComponent(x: number): string {
-        throw new Error("needs implementation");
+        // FIXME: assume unescaped representation is fine
+        // FIXME: assume oob behavior is fine
+        this.checkBounds(x);
+        return StringName.unescapedArray(this.name, this.delimiter)[x];
     }
 
     public setComponent(n: number, c: string): void {
-        throw new Error("needs implementation");
+        // FIXME: assume oob behavior is fine
+        this.checkBounds(n);
+        let arr: string[] = StringName.escapedArray(this.name, this.delimiter);
+        arr[n] = c;
+        this.name = arr.join(this.delimiter);
     }
 
     public insert(n: number, c: string): void {
-        throw new Error("needs implementation");
+        // FIXME: assume oob behavior is fine
+        if (n != this.length) {
+            this.checkBounds(n);
+        }
+        let arr: string[] = StringName.escapedArray(this.name, this.delimiter);
+        arr.splice(n, 0, c);
+        this.name = arr.join(this.delimiter);
+        this.length++;
     }
 
     public append(c: string): void {
-        throw new Error("needs implementation");
+        this.name += this.delimiter + c;
+        this.length++;
     }
 
     public remove(n: number): void {
-        throw new Error("needs implementation");
+        // FIXME: assume oob behavior is fine
+        this.checkBounds(n);
+        let arr: string[] = StringName.escapedArray(this.name, this.delimiter);
+        arr.splice(n, 1);
+        this.name = arr.join(this.delimiter);
+        this.length--;
     }
 
+    // FIXME: not in interface
     public concat(other: Name): void {
-        throw new Error("needs implementation");
+        this.name += this.delimiter + other.asDataString();
+        this.length += other.getNoComponents();
     }
 
 }
